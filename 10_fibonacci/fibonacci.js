daab3628581f3f2b7aa5a89b86a9b84566114871
dania-afridi <dania.awais33@gmail.com>
2022-09-05 19:02:59 +0200

    fibonacci functionality added and updated files

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let series = [1,1,2,3,5,8];
+    let isInt = Number.isInteger(num);
 
+    if(!isInt){
+        num=parseInt(num);
+    }
+
+    if(num<0){
+        return "OOPS"
+    }else if(series[num-1]){
+        return series[num-1];
+    }else{
+        do{
+            series.push(series[series.length-2]+series[series.length-1]);
+        }while(!series[num-1]);
+        return series[num-1];
+    }
 };
 
 // Do not edit below this line
