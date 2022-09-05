fc54ade836fc2585eaa826dbda3c853c7036dd35
dania-afridi <dania.awais33@gmail.com>
2022-09-05 18:18:54 +0200

    palindromes updated

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let realStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    let reverseStr = realStr.split('').reverse().join('');
+    if (realStr === reverseStr){
+        return true;
+    } else{
+        return false;
+    }
 };
 
 // Do not edit below this line
