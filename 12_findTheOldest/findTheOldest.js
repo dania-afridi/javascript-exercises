61f2de788d135e1a81e771afee5c96cd64d8434c
dania-afridi <dania.awais33@gmail.com>
2022-09-05 19:41:15 +0200

    modified

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let today = new Date().getFullYear();
+    let index=0;
 
+    let personsAge;
+    let currentAge;
+
+    if(arr[index].yearOfDeath){
+        personsAge =arr[index].yearOfDeath-arr[index].yearOfBirth;
+    }else{
+        personsAge = today - arr[index].yearOfBirth;
+    }
+    
+    for(let i=1; i<arr.length; i++){
+        if(arr[i].yearOfDeath){
+            currentAge =arr[i].yearOfDeath-arr[i].yearOfBirth;
+        }else{
+            currentAge = today -arr[i].yearOfBirth;
+        }
+
+        if(personsAge < currentAge){
+            index=i;
+            personsAge = currentAge;
+        }
+    }
+
+    return arr[index];
 };
 
 // Do not edit below this line
